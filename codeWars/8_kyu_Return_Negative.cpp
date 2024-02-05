db9c4bfdd3243bb558d7756879c7c1b39e1eacc3
AhmadOthman <trollingosman101@outlook.com>
2024-02-05 13:54:06 +0200

    -  feat(Return_Negative.cpp): add code for returning negative of a number

---
+++ codeWars/8_kyu_Return_Negative.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int makeNegative(int num)
+{
+    return (num > 0) ? -num : num;
+}
+
+int main()
+{
+    cout
+        << makeNegative(5) << "\n"
+        << makeNegative(-5) << "\n"
+        << makeNegative(0) << endl;
+}
