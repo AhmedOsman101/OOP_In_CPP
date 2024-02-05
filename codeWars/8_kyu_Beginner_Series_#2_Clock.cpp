9b189b511be809a7a9c2e959c2f9d4eb3b98bf48
AhmadOthman <trollingosman101@outlook.com>
2024-02-05 14:54:54 +0200

    feat: Add clock time converter function
    This change adds a new function called `past` that calculates the total milliseconds past midnight based on given hours `h`, minutes `m`, and seconds `s`.
    
    The addition of this function allows for convenient conversion between clock time and milliseconds, enabling future time calculations.

---
+++ codeWars/8_kyu_Beginner_Series_#2_Clock.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int past(int h, int m, int s)
+{
+    return (h * 60 * 60 * 1000) + (m * 60 * 1000) + (s * 1000);
+}
+
+int main()
+{
+    cout << past(0, 1, 1) << endl;
+}
