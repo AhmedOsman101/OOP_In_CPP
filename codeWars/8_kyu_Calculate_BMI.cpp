d8d2b0841a140267adc510d47cf2fc58f06d92d4
AhmadOthman <trollingosman101@outlook.com>
2024-02-05 14:38:04 +0200

    -  feat(Calculate_BMI.cpp): implement BMI calculation function and main function for testing

---
+++ codeWars/8_kyu_Calculate_BMI.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+string bmi(double w, double h)
+{
+    double bmi = w / pow(h, 2);
+
+    if (bmi <= 18.5)
+        return "Underweight";
+
+    if (bmi <= 25.0)
+        return "Normal";
+
+    if (bmi <= 30.0)
+        return "Overweight";
+
+    if (bmi > 30.0)
+        return "Obese";
+}
+
+int main()
+{
+    cout
+        << bmi(50, 1.80)
+        << "\n"
+        << endl;
+}
