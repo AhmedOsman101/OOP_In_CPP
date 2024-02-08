c7e44a6c5b46cd236f44032c7e0b865bc2bfa8cc
AhmadOthman <trollingosman101@outlook.com>
2024-02-08 02:41:07 +0200

    .

--- Lectures/chapter1/mixed.cpp
+++ Lectures/chapter1/mixed.cpp
@@ -5,7 +5,7 @@ int main() {
     int count = 7;
     float avgWeight = 155.5F;
     double totalWeight = count * avgWeight;
-    cout << "totalWeight =" << totalWeight << endl;
+    cout << "totalWeight = " << totalWeight << endl;
     cout << "totalWeight size in bytes: " << sizeof(totalWeight) << endl;
     return 0;
 }
