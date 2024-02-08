a13d842e1cea83c76a7b8f40aaf217124cfcb223
AhmadOthman <trollingosman101@outlook.com>
2024-02-08 02:40:13 +0200

    -  feat(chapter1): add mixed.cpp file -  feat(chapter1): add signtest.cpp file

---
+++ Lectures/chapter1/signtest.cpp
@@ -0,0 +1,12 @@
+// tests signed and unsigned integers
+#include <iostream>
+using namespace std;
+int main() {
+    int signedVar = 1500000000;                  // signed
+    unsigned int unsignedVar = 1500000000;       // unsigned
+    signedVar = (signedVar * 2) / 3;             // calculation exceeds range
+    unsignedVar = (unsignedVar * 2) / 3;         // calculation within range
+    cout << "signedVar = " << signedVar << endl; // wrong
+    cout << "unsignedVar = " << unsignedVar << endl; // OK
+    return 0;
+}
