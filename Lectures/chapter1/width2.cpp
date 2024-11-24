4542bcc9943c9c0fbb0135c0a8f02358132a19d3
AhmedOsman101 <trollingosman101@outlook.com>
2024-11-24 10:57:03 +0200

    update tasks.json and width2.cpp

--- Lectures/chapter1/width2.cpp
+++ Lectures/chapter1/width2.cpp
@@ -4,7 +4,8 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
     long pop1 = 2425785, pop2 = 47, pop3 = 9761;
     cout << setw(8) << "LOCATION" << setw(12) << "POPULATION" << endl
         << setw(8) << "Portcity" << setw(12) << pop1 << endl
@@ -12,3 +13,4 @@ int main() {
         << setw(8) << "Lowville" << setw(12) << pop3 << endl;
     return 0;
 }
+
